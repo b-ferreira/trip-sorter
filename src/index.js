907289a4934c51864e3107b95a9650979f9ace33
Bruno Ferreira Assis <bruno.assis@ifood.com.br>
2018-10-04 01:35:34 -0300

    Load store provider and style base on index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import 'style/base';
+
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-const AppRoot = () => <div>Property Finder!</div>;
+import AppRoot from 'modules/App';
+
+import Store from './store';
+
 const appNode = document.querySelector('#app');
 
 render(
-  <AppRoot />,
+  <Provider store={Store}>
+    <AppRoot />
+  </Provider>,
   appNode
 );
