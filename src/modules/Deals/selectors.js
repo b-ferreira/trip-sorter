66ec241cc1b88815e7c172d2ec6eb83e463ad194
Bruno Ferreira Assis <bruno.assis@ifood.com.br>
2018-10-04 21:21:22 -0300

    Create selector for available cities using deals data

--- src/modules/Deals/selectors.js
+++ src/modules/Deals/selectors.js
@@ -1,3 +1,4 @@
+import { concat, map, prop, uniq } from 'ramda';
 import { createSelector } from 'reselect';
 
 const dealsBaseState = state => state.deals;
@@ -5,4 +6,11 @@ const dealsBaseState = state => state.deals;
 export const dealsListSelector = createSelector(
   dealsBaseState,
   ({ data }) => data || []
+);
+
+const allDepartureCities = map(prop('departure'));
+const allArrivalCities = map(prop('arrival'));
+export const availableCitiesSelector = createSelector(
+  dealsBaseState,
+  ({ data }) => data ? uniq(concat(allDepartureCities(data), allArrivalCities(data))) : []
 );
