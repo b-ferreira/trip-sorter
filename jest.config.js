ae431eb87bb52ae7b0e44e550a76bd34b2048973
Bruno Ferreira Assis <bruno.assis@ifood.com.br>
2018-10-07 23:21:11 -0300

    Add and configure Jest and Enzyme

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  setupTestFrameworkScriptFile: "jest-enzyme",
+  testEnvironment: "enzyme",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    "^components(.*)$": "<rootDir>/src/components$1",
+    "^constants(.*)$": "<rootDir>/src/constants$1",
+    "^fixtures(.*)$": "<rootDir>/__fixtures__$1",
+    "^images(.*)$": "<rootDir>/images$1",
+    "^middlewares(.*)$": "<rootDir>/src/middlewares$1",
+    "^modules(.*)$": "<rootDir>/src/modules$1",
+    "^reducers(.*)$": "<rootDir>/src/reducers$1",
+    "^services(.*)$": "<rootDir>/src/services$1",
+    "^utils(.*)$": "<rootDir>/src/utils$1"
+  }
+};
